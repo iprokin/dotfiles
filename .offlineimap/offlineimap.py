7a031ab02ecb9b5be589a20f1cf3c6c521f58a33
Ilya on ASUS laptop <isprokin@gmail.com>
2016-05-19 18:41:22 +0200

    dotfiles update; xcape in .xinitrc

--- .offlineimap/offlineimap.py
+++ .offlineimap/offlineimap.py
@@ -4,6 +4,9 @@ import os
 
 def get_pass(storeinfile='/tmp/offlineimap_ip_gm.tmp'):
     if not os.path.isfile(storeinfile):
-        call('pass email/offlineimap/isprokin@gmail.com > {}'.format(storeinfile), shell=True)
+        call('pass email/offlineimap/isprokin@gmail.com > {0} && chmod 600 {0}'.format(storeinfile), shell=True)
     #return check_output('pass email/offlineimap/isprokin@gmail.com', shell=True).strip('\n')
     return check_output('cat {}'.format(storeinfile), shell=True).strip('\n')
+
+if __name__ == '__main__':
+    get_pass()
